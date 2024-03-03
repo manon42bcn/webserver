ec4b8e157b9ce05a15ecf1a56e93fc2ef6c32d27
Victor Aguilar <veaz.24@gmail.com>
2024-03-03 13:35:16 +0100

    checkArgs function created

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,35 @@
 /*   By: vaguilar <vaguilar@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:03:40 by vaguilar          #+#    #+#             */
-/*   Updated: 2024/03/03 13:21:44 by vaguilar         ###   ########.fr       */
+/*   Updated: 2024/03/03 13:34:12 by vaguilar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
 #include <iostream>
+#include <fstream>
 
-int main(int argc, char const *argv[]) {
+bool checkArgs(int argc, char **argv)
+{
+    std::ifstream file;
+    file.open(argv[1]);
 
-    if (argc < 2 || argc > 3)
-		std::cout << "Args error. Type --help for instructions." << std::endl;
-	if (argc == 3)
-		std::cout << "invalid argument" << std::endl;
+    if (argc < 2)
+    {
+        std::cout << "Args error. Type --help for instructions." << std::endl;
+        return (false);
+    }
+    if (!file)
+    {
+        std::cout << "Error: file \"" << argv[1] << "\" cant be opened." << std::endl;
+        return (false);
+    }
+    return (true);
+}
 
-    if(argv[1] == (std::string)"hi") // Testing the compare strings
-        std::cout << "Hi" << argv[0] << std::endl;
+int main(int argc, char *argv[]) {
+
+    if (!checkArgs(argc, argv))
+        exit(1);
 
 }
