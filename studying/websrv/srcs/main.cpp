92b3f79e749fb886f2861fd3150c70b643b97193
Manuel Porras Ojeda <manon42bcn@yahoo.com>
2024-09-30 22:35:22 +0200

    working on several ports

--- studying/websrv/srcs/main.cpp
+++ studying/websrv/srcs/main.cpp
@@ -11,119 +11,127 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <poll.h>
+#include <fcntl.h>
 #include <cstring>
 #include <cstdlib>
-#include <unistd.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <poll.h>
 
-#define PORT 8080
+#define PORT1 8080
+#define PORT2 9090
 #define MAX_CLIENTS 100
+#define BUFFER_SIZE 1024
 
-int set_non_blocking(int fd) {
+int set_nonblocking(int fd) {
 	int flags = fcntl(fd, F_GETFL, 0);
-	if (flags == -1) {
-		std::cerr << "Error al obtener flags del socket" << std::endl;
+	if (flags == -1) return -1;
+	return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
+
+int create_server_socket(int port) {
+	int server_fd;
+	struct sockaddr_in address;
+
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		std::cerr << "Error al crear el socket." << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	flags |= O_NONBLOCK;
-	if (fcntl(fd, F_SETFL, flags) == -1) {
-		std::cerr << "Error al configurar el socket como no bloqueante" << std::endl;
+
+	int opt = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+		std::cerr << "Error al configurar el socket." << std::endl;
+		close(server_fd);
 		exit(EXIT_FAILURE);
 	}
-	return 0;
+
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(port);
+
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		std::cerr << "Error al hacer bind en el puerto " << port << std::endl;
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(server_fd, 3) < 0) {
+		std::cerr << "Error al escuchar en el puerto " << port << std::endl;
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	std::cout << "Servidor escuchando en el puerto " << port << std::endl;
+	return server_fd;
 }
 
 int main() {
-	int server_fd, new_socket;
-	struct sockaddr_in address;
-	int addrlen = sizeof(address);
+	int server_fd1 = create_server_socket(PORT1);
+	int server_fd2 = create_server_socket(PORT2);
 
-	// Crear el socket
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-		std::cerr << "Error al crear el socket" << std::endl;
-		exit(EXIT_FAILURE);
-	}
+	set_nonblocking(server_fd1);
+	set_nonblocking(server_fd2);
 
-	// Configurar la dirección y el puerto
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+	struct pollfd poll_fds[MAX_CLIENTS];
+	int nfds = 2;
 
-	// Vincular el socket al puerto
-	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-		std::cerr << "Error en bind()" << std::endl;
-		close(server_fd);
-		exit(EXIT_FAILURE);
-	}
+	poll_fds[0].fd = server_fd1;
+	poll_fds[0].events = POLLIN;
 
-	// Escuchar en el puerto
-	if (listen(server_fd, 10) < 0) {
-		std::cerr << "Error en listen()" << std::endl;
-		close(server_fd);
-		exit(EXIT_FAILURE);
-	}
+	poll_fds[1].fd = server_fd2;
+	poll_fds[1].events = POLLIN;
 
-	// Configurar el socket del servidor como no bloqueante
-	set_non_blocking(server_fd);
-
-	// Crear estructura poll
-	struct pollfd fds[MAX_CLIENTS];
-	fds[0].fd = server_fd;  // Descriptor del socket del servidor
-	fds[0].events = POLLIN; // Interesado en el evento de lectura
-	int nfds = 1;           // Número de file descriptors activos
-
-	std::cout << "Servidor escuchando en el puerto " << PORT << std::endl;
+	char buffer[BUFFER_SIZE];
 
 	while (true) {
-		// Llamada a poll() para verificar actividad en los sockets
-		int poll_count = poll(fds, nfds, -1);
+		int poll_count = poll(poll_fds, nfds, -1);
 
-		if (poll_count == -1) {
-			std::cerr << "Error en poll()" << std::endl;
-			close(server_fd);
+		if (poll_count < 0) {
+			std::cerr << "Error en poll()." << std::endl;
 			exit(EXIT_FAILURE);
 		}
 
-		// Verificar si el socket del servidor tiene una nueva conexión
-		if (fds[0].revents & POLLIN) {
-			if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) >= 0) {
-				// Configurar el nuevo socket como no bloqueante
-				set_non_blocking(new_socket);
-				std::cout << "Nueva conexión establecida en el socket " << new_socket << std::endl;
+		for (int i = 0; i < nfds; ++i) {
+			if (poll_fds[i].revents & POLLIN) {
+				if (poll_fds[i].fd == server_fd1 || poll_fds[i].fd == server_fd2) {
+					int new_socket;
+					struct sockaddr_in address;
+					socklen_t addrlen = sizeof(address);
 
-				// Agregar el nuevo socket al array de poll
-				fds[nfds].fd = new_socket;
-				fds[nfds].events = POLLIN;
-				nfds++;
-			}
-		}
+					new_socket = accept(poll_fds[i].fd, (struct sockaddr *)&address, &addrlen);
+					if (new_socket < 0) {
+						std::cerr << "Error al aceptar conexión." << std::endl;
+						continue;
+					}
 
-		// Verificar actividad en los sockets de los clientes
-		for (int i = 1; i < nfds; i++) {
-			if (fds[i].revents & POLLIN) {
-				char buffer[1024] = {0};
-				int valread = read(fds[i].fd, buffer, 1024);
-				if (valread > 0) {
-					std::cout << "Mensaje recibido: " << buffer << std::endl;
+					std::cout << "Nueva conexión aceptada en el puerto "
+					          << (poll_fds[i].fd == server_fd1 ? PORT1 : PORT2)
+					          << std::endl;
 
-					// Enviar una respuesta simple
-					std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!";
-					send(fds[i].fd, response.c_str(), response.length(), 0);
-				} else if (valread == 0) {
-					// El cliente cerró la conexión
-					std::cout << "Conexión cerrada en el socket " << fds[i].fd << std::endl;
-					close(fds[i].fd);
-					// Eliminar el socket del array de poll
-					fds[i] = fds[nfds - 1];
-					nfds--;
+					set_nonblocking(new_socket);
+					poll_fds[nfds].fd = new_socket;
+					poll_fds[nfds].events = POLLIN;
+					nfds++;
+				} else {
+					int valread = read(poll_fds[i].fd, buffer, BUFFER_SIZE);
+					if (valread == 0) {
+						std::cout << "Cliente desconectado." << std::endl;
+						close(poll_fds[i].fd);
+						poll_fds[i] = poll_fds[nfds - 1];
+						nfds--;
+					} else if (valread > 0) {
+						buffer[valread] = '\0';
+						std::cout << "Mensaje recibido: " << buffer << std::endl;
+						std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!";
+						send(poll_fds[i].fd, response.c_str(), response.length(), 0);
+					}
 				}
 			}
 		}
 	}
 
-	close(server_fd);
+	close(server_fd1);
+	close(server_fd2);
 	return 0;
 }
