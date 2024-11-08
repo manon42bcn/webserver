3b740ef47356efdce2a04051aa171cdfadb33bfa
Manuel Porras Ojeda <manuel.porrasojeda@telefonica.com>
2024-11-08 18:10:45 +0100

    mini fix

--- studying/websrv/inc/WebserverCache.hpp
+++ studying/websrv/inc/WebserverCache.hpp
@@ -19,16 +19,14 @@
 struct CacheEntry {
 	std::string url;
 	std::string content;
-	int access_count;
 
 	CacheEntry(const std::string &u, const std::string &c)
-			: url(u), content(c), access_count(1) {}
+			: url(u), content(c) {};
 };
 
 class WebServerCache {
 	private:
 		size_t _capacity;
-//		int _min_access_count;
 		std::list<CacheEntry> _entries;
 		std::map<std::string, std::list<CacheEntry>::iterator> _cache_map;
 	public:
