95e761057a403dd89ab1bcdbd376cba891bb7fd5
Manuel Porras Ojeda <manon42bcn@yahoo.com>
2024-11-18 00:40:24 +0100

    improve autoindex

---
+++ websrv/inc/ws_general_defines.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ws_general_defines.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 00:31:04 by mporras-          #+#    #+#             */
+/*   Updated: 2024/11/18 00:33:34 by mporras-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WS_GENERAL_DEFINES_HPP
+#define WS_GENERAL_DEFINES_HPP
+
+#define AUTOINDEX_STYLE \
+"body { background-color: #fcf8ea; font-family: Arial, Helvetica, sans-serif; height: 100%; width: 100%;} table { width: 100%; border-collapse: collapse; font-family: Arial, sans-serif; margin: 20px 0;}"\
+"th, td { text-align: left; padding: 8px;}"\
+"tbody tr {border-bottom: 1px solid #2899fa;}"\
+"tbody tr:hover {background-color: #f1f1f1;}"\
+"th {background-color: #f4f4f4;border-bottom: 2px solid #2899fa;font-weight: bold;}"
+#define FOOTER_STYLE "footer {text-align: center;background-color: #ffffff;padding: 10px 0;border-top: 5px solid #2899fa;border-bottom: 1px solid #2899fa;font-size: 14px;}"
+#define FOOTER_GENERAL "<footer>Powered by WebServer</footer>"
+
+#endif
--- websrv/srcs/HttpAutoIndex.cpp
+++ websrv/srcs/HttpAutoIndex.cpp
@@ -6,7 +6,7 @@
 /*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 22:09:10 by mporras-          #+#    #+#             */
-/*   Updated: 2024/11/17 23:14:11 by mporras-         ###   ########.fr       */
+/*   Updated: 2024/11/18 00:39:52 by mporras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,61 +42,65 @@ bool HttpAutoIndex::handle_request() {
 }
 
 void HttpAutoIndex::get_file_content(std::string& path) {
-	DIR* dir = opendir(path.c_str());
-	if (dir == NULL) {
-		turn_off_sanity(HTTP_NOT_FOUND,
-						"Dir cannot be open.");
-		return ;
-	}
-	std::string path_to_file = _request.path;
-	if (path_to_file[path_to_file.size() - 1] != '/') {
-		path_to_file += "/";
-	}
-	std::ostringstream out;
-	out << "<!DOCTYPE html>\n<html>\n<head>\n<title>Index of " << _request.path << "</title>\n";
-	out << "<style>body { font-family: Arial, sans-serif; } table { border-collapse: collapse; } th, td { border: 1px solid #ddd; padding: 8px; } th { text-align: left; }</style>\n";
-	out << "</head>\n<body>\n";
-	out << "<h1>Index of " << _request.path << "</h1>\n";
-	out << "<table>\n<tr><th>Name</th><th>Size</th><th>Last Modified</th></tr>\n";
+	try {
+		DIR* dir = opendir(path.c_str());
+		if (dir == NULL) {
+			turn_off_sanity(HTTP_NOT_FOUND,
+			                "Dir cannot be open.");
+			return ;
+		}
+		std::string path_to_file = _request.path;
+		if (path_to_file[path_to_file.size() - 1] != '/') {
+			path_to_file += "/";
+		}
+		std::ostringstream out;
+		out << "<!DOCTYPE html>\n<html>\n<head>\n<title>Index of " << _request.path << "</title>\n";
+		out << "<style>" << AUTOINDEX_STYLE << FOOTER_STYLE << "</style>\n";
+		out << "</head>\n<body>\n";
+		out << "<h1>AutoIndex of : " << _request.path << "</h1>\n";
+		out << "<table>\n<tr><th>Name</th><th>Size</th><th>Last Modified</th></tr>\n";
 
-	struct dirent* entry;
-	while ((entry = readdir(dir)) != NULL) {
-		std::string name = entry->d_name;
+		struct dirent* entry;
+		while ((entry = readdir(dir)) != NULL) {
+			std::string name = entry->d_name;
+			if (name == "." || name == "..") {
+				continue;
+			}
+			std::string base_path = _request.normalized_path + name;
+			struct stat file_stat;
+			if (stat(base_path.c_str(), &file_stat) == -1) {
+				_log->log_error(AI_NAME,
+				                "Error reading file.");
+				continue;
+			}
+			out << "<tr>";
+			out << "<td><a href=\"" << path_to_file << name << "\">" << name << "</a></td>";
+			if (S_ISDIR(file_stat.st_mode)) {
+				out << "<td>Directory</td>";
+			} else {
+				std::ostringstream size_stream;
+				size_stream << file_stat.st_size;
+				out << "<td>" << size_stream.str() << " bytes</td>";
+			}
+			char time_buf[80];
+			std::strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", std::localtime(&file_stat.st_mtime));
+			out << "<td>" << time_buf << "</td>";
 
-		if (name == "." || name == "..") {
-			continue;
+			out << "</tr>\n";
 		}
 
-		std::string base_path = _request.normalized_path + name;
-
-		struct stat file_stat;
-		if (stat(base_path.c_str(), &file_stat) == -1) {
-			_log->log_error(AI_NAME,
-							"Error reading file.");
-			continue;
-		}
-
-		out << "<tr>";
-		out << "<td><a href=\"" << path_to_file << name << "\">" << name << "</a></td>";
-
-		if (S_ISDIR(file_stat.st_mode)) {
-			out << "<td>Directory</td>";
-		} else {
-			std::ostringstream size_stream;
-			size_stream << file_stat.st_size;
-			out << "<td>" << size_stream.str() << " bytes</td>";
-		}
-		char time_buf[80];
-		std::strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", std::localtime(&file_stat.st_mtime));
-		out << "<td>" << time_buf << "</td>";
-
-		out << "</tr>\n";
+		out << "</table>\n" << FOOTER_GENERAL << "</body>\n</html>";
+		closedir(dir);
+		_response_data.content = out.str();
+		_response_data.status = true;
+	} catch (std::exception& e) {
+		std::ostringstream detail;
+		detail << "Error Creating Autoindex file for " << path << ". Error: " << e.what();
+		_log->log_warning(AI_NAME,detail.str());
+		turn_off_sanity(HTTP_INTERNAL_SERVER_ERROR,
+						"Error Creating Autoindex data.");
+		_response_data.status = false;
 	}
-
-	out << "</table>\n</body>\n</html>";
-	closedir(dir);
-	_response_data.content = out.str();
-	_response_data.status = true;
 }
 
 void HttpAutoIndex::get_file_content(int pid, int (&fd)[2]) {
