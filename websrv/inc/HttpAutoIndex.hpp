0853067ec14da9f085d3ec2fea32d37595f5d2d1
Manuel Porras Ojeda <manon42bcn@yahoo.com>
2024-11-18 00:47:18 +0100

    Autoindex documented

--- websrv/inc/HttpAutoIndex.hpp
+++ websrv/inc/HttpAutoIndex.hpp
@@ -6,7 +6,7 @@
 /*   By: mporras- <manon42bcn@yahoo.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 22:06:14 by mporras-          #+#    #+#             */
-/*   Updated: 2024/11/18 00:33:42 by mporras-         ###   ########.fr       */
+/*   Updated: 2024/11/18 00:41:47 by mporras-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,26 @@
 #include "WebServerResponseHandler.hpp"
 #define AI_NAME "HttpAutoIndex"
 
+/**
+ * @brief The `HttpAutoIndex` class handles the generation of an automatic
+ * 		  index for a directory listing.
+ *
+ * This class inherits from `WsResponseHandler` and is responsible for providing
+ * a response that contains a directory index when requested.
+ * It includes methods for reading the content of a directory and generating
+ * the appropriate response.
+ */
 class HttpAutoIndex : public WsResponseHandler {
-
-	virtual void get_file_content(int pid, int (&fd)[2]);
-	virtual void get_file_content(std::string& path);
-public:
-	HttpAutoIndex (const LocationConfig *location,
-				   const Logger *log,
-				   ClientData* client_data,
-				   s_request& request,
-				   int fd);
-//	bool handle_get();
-	bool handle_request();
+	private:
+		virtual void get_file_content(int pid, int (&fd)[2]);
+		virtual void get_file_content(std::string& path);
+	public:
+		HttpAutoIndex (const LocationConfig *location,
+					   const Logger *log,
+					   ClientData* client_data,
+					   s_request& request,
+					   int fd);
+		bool handle_request();
 };
 
 #endif
