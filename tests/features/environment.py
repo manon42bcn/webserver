68e469b307bf2e115bb0fe6b6c462fa1acb0e441
Manuel Porras Ojeda <manuel.porrasojeda@telefonica.com>
2024-12-02 14:55:08 +0100

    Test logger

---
+++ tests/features/environment.py
@@ -0,0 +1,23 @@
+import logging
+import os
+
+def before_all(context):
+    log_dir = "_output"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    log_file = os.path.join(log_dir, "behave_execution.log")
+    logger = logging.getLogger("WEBSERVER")
+    logger.setLevel(logging.DEBUG)
+
+    file_handler = logging.FileHandler(log_file, mode='w')
+    file_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '[%(asctime)s][%(levelname)s]: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    context.logger = logger
+    logger.info("Logger start.")
--- tests/features/steps/steps.py
+++ tests/features/steps/steps.py
@@ -6,12 +6,11 @@ from bs4 import BeautifulSoup
 import warnings
 from urllib3.exceptions import NotOpenSSLWarning
 
-warnings.filterwarnings("ignore", category=NotOpenSSLWarning)
-
 @step('set connection and headers for ip "{ip}" port "{port}" and domain "{domain}"')
 def set_headers_before_request(context, ip, port, domain):
     context.connection = http.client.HTTPConnection(ip, int(port))
     context.headers = {"Host": domain}
+    context.logger.debug(f"Connection and headers set up for ip {ip}, port {port} and domain {domain} has been set up.")
 
 @step('send a "{method}" request to "{location}" using set up domain and headers and status code "{status_code}"')
 def send_request_using_host(context, method, location, status_code):
@@ -19,16 +18,19 @@ def send_request_using_host(context, method, location, status_code):
     response = context.connection.getresponse()
     assert response.status == int(status_code)
     context.html_content = response.read().decode()
+    context.logger.debug(f"Request response: {context.html_content}")
 
 @step('send a request to "{url}" and get code status "{code_status}"')
 def send_request_to_url(context, url, code_status):
     response = requests.get(url)
     assert response.status_code == int(code_status), f"Failed to fetch page: {response.status_code}"
     context.html_content = response.text
+    context.logger.debug(f"Request response: {context.html_content}")
 
 @step('I parse html response body')
 def parse_html_response(context):
     context.soup = BeautifulSoup(context.html_content, 'html.parser')
+    context.logger.debug("Content has been parsed.")
 
 @step('The response body content includes "{label}" with content "{content}"')
 def assert_element_content_by_label(context, label, content):
@@ -36,6 +38,7 @@ def assert_element_content_by_label(context, label, content):
     found = False
     for elements in find_elements:
         if elements.text == content:
+            context.logger.debug(f"Element has been found.: {elements.text}")
             found = True
             break
     assert found, f"Element {label} with content {content} has been not found."
